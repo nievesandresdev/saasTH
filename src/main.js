cf88ffabca0bc669d5b8ebd58c81f93a1b61432b
xpestana <unibasileaweb@gmail.com>
2024-06-14 17:52:25 -0400

    config hoster

--- src/main.js
+++ src/main.js
@@ -1,9 +1,30 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import { createHead } from '@vueuse/head'
 
-const app = createApp(App);
+import './assets/css/style.css'; 
+import './assets/css/custom.css'; 
+import './assets/css/buttons.css'; 
+import './assets/css/inputs.css'; 
+import './assets/css/colors.css';
 
-app.use(router);
+import './assets/css/tailwind.css';
 
-app.mount('#app');
+import { i18n }  from './i18n'
+import { pinia } from './stores';
+
+const head = createHead()
+
+function initializeApp () {
+    const app = createApp(App);
+
+    app.use(pinia)
+    app.use(i18n)
+    app.use(router)
+    app.use(head)
+    app.mount('#app')
+
+}
+
+initializeApp()
