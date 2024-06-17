393b73bf628e6a4c7e779ed3e9485d71bbac7e9e
xpestana <unibasileaweb@gmail.com>
2024-06-17 08:07:59 -0400

    sdfsf

---
+++ src/api/config/apiRoute.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+
+export const authPath = 'auth'
+
+export const homePath = 'home'
+export const hotelPath = 'hotel'
+export const staySurveyPath = 'staySurvey'
+export const hotelOtaPath = 'hotelOta'
+export const experiencePath = 'experience'
+export const placePath = 'place'
+export const utilityPath = 'utility'
+export const cityPath = 'city'
+export const stayPath = 'stay'
+export const guestPath = 'guest'
+export const chatPath = 'chat'
+export const utilsPath = 'utils'
+export const facilityPath = 'facility'
+export const queryPath = 'query'
+export const querySettingsPath = 'query-settings'
+export const stayAccessPath = 'stayAccess'
+export const requestSettingsPath = 'request-settings'
