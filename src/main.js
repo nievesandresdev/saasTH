2c4ce22e56f0f49afc23b2dafb21e2ed52657823
Andres <andresdreamerf@gmail.com>
2024-06-18 17:57:43 -0400

    fix

--- src/main.js
+++ src/main.js
@@ -3,22 +3,11 @@ import App from './App.vue'
 import router from './router';
 import { createHead } from '@vueuse/head'
 
-import './assets/css/style.css'; 
-import './assets/css/custom.css'; 
-import './assets/css/buttons.css'; 
-import './assets/css/inputs.css'; 
-import './assets/css/colors.css';
-//estilos generales
-import './assets/css/main.css'
-
-
 //estilos generales
 import './assets/css/main.css'
 
 
 const app = createApp(App);
-
-import './assets/css/tailwind.css';
 
 import { i18n }  from './i18n'
 import { pinia } from './stores';
