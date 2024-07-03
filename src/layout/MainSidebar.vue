1ae0f28939e46871e7e4a230919c09632921b89e
Fran <francisco20990@gmail.com>
2024-07-02 21:14:03 -0400

    active main select panel user

--- src/layout/MainSidebar.vue
+++ src/layout/MainSidebar.vue
@@ -15,12 +15,12 @@
       <!-- links -->
       <router-link
         class="rounded-[10px] hbg-green-200 flex items-center p-2 mt-6"
-        :class="{'hbg-green-600 shadow-lg':$route.name == 'DashboardIndex','hover-gray-100':$route.name !== 'DashboardIndex'}"
+        :class="{'hbg-green-600 shadow-lg': $route.name === 'DashboardIndex', 'hover-gray-100': $route.name !== 'DashboardIndex'}"
         to="/dashboard"
       >
-        <img class="w-6 h-6" src="/assets/icons/1.TH.DASHBOARD.svg" :class="{'icon-white':$route.name == 'DashboardIndex'}">
+        <img class="w-6 h-6" src="/assets/icons/1.TH.DASHBOARD.svg" :class="{'icon-white': $route.name === 'DashboardIndex'}">
         <div :class="widthMenu">
-          <p class="text-sm font-semibold ml-2 whitespace-nowrap text-left leading-[120%]" :class="{'text-white':$route.name == 'DashboardIndex'}">
+          <p class="text-sm font-semibold ml-2 whitespace-nowrap text-left leading-[120%]" :class="{'text-white': $route.name === 'DashboardIndex'}">
             Dashboard
           </p>
         </div>
@@ -40,11 +40,11 @@
             v-for="(link, indexLink) in section.group" :key="indexLink"
             :to="link.url"
             class="rounded-[10px] flex items-center p-2"
-            :class="{'hbg-green-600 shadow-lg':link.include.includes($route.name),'hover-gray-100':!link.include.includes($route.name)}"
+            :class="{'hbg-green-600 shadow-lg': link.include.includes($route.name), 'hover-gray-100': !link.include.includes($route.name)}"
           >
-            <img class="w-6 h-6" src="/assets/icons/1.TH.DASHBOARD.svg" :class="{'icon-white':link.include.includes($route.name)}">
+            <img class="w-6 h-6" src="/assets/icons/1.TH.DASHBOARD.svg" :class="{'icon-white': link.include.includes($route.name)}">
             <div :class="widthMenu">
-              <p class="text-sm font-semibold ml-2 whitespace-nowrap text-left leading-[120%]" :class="{'text-white':link.include.includes($route.name)}">{{link.title}}</p>
+              <p class="text-sm font-semibold ml-2 whitespace-nowrap text-left leading-[120%]" :class="{'text-white': link.include.includes($route.name)}">{{link.title}}</p>
             </div>
           </router-link>
         </div>
@@ -56,16 +56,16 @@
           v-for="(button, indexButton) in user_buttons" :key="indexButton"
           @click="button.action && button.action()"
           class="rounded-[10px] flex items-center p-1 max-h-[40px]"
-          :class="{'hbg-green-600 shadow-lg':false,'hover-gray-100':false}"
+          :class="{'hbg-green-600 shadow-lg': button.active, 'hover-gray-100': !button.active}"
         >
           <img 
             class="w-8 h-8 rounded-full" :src="button.icon"
-            :class="{'icon-white':false,'border border-white':false}"
+            :class="{'icon-white': false, 'border border-white': button.active}"
           >
           <div :class="widthMenu">
             <p 
               class="text-sm font-semibold ml-2 text-left leading-[120%]"
-              :class="{'text-white': false}"
+              :class="{'text-white': button.active}"
             >{{button.title}}</p>
           </div>
         </button>
@@ -92,7 +92,7 @@
               <img src="/assets/icons/1.TH.SUSCRIPCIÓN.svg" alt="Ver Suscripción" class="h-5 w-5 mb-1 mr-2 icon-hover" />
               Ver Suscripción
             </button>
-            <button class="modal-button flex items-center justify-center py-2 text-[#8A8A8A] font-bold text-xs hover:text-[#34A98F]">
+            <button @click="redirectToUserPanel" class="modal-button flex items-center justify-center py-2 text-[#8A8A8A] font-bold text-xs hover:text-[#34A98F]">
               <img src="/assets/icons/1.TH.Settings.svg" alt="Ajustes de cuenta" class="h-5 w-5 mr-2 icon-hover" />
               Ajustes de cuenta
             </button>
@@ -113,120 +113,123 @@
 
 <script setup>
 import { ref, computed } from 'vue'
-import { useRoute,useRouter } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores/modules/auth/login'
-import ModalWindow from '@/components/ModalWindow.vue';
+import ModalWindow from '@/components/ModalWindow.vue'
 
-const route = useRoute();
-  const router = useRouter();
-const authStore = useAuthStore() 
+const route = useRoute()
+const router = useRouter()
+const authStore = useAuthStore()
 
 const modalProfile = ref(false)
 
+function redirectToUserPanel() {
+  router.push({ name: 'UserPanel' })
+  closeModalProfile()
+}
+
 const closeModalProfile = () => {
-    modalProfile.value = false
+  modalProfile.value = false
 }
 
 const menu_links = ref([
-    {
-        title: 'Operación',
-        group: [
-            {
-                title: 'Estancias',
-                icon: '1.TH.DASHBOARD',
-                include : ['-'],
-                url : '/dashboard'
-            },
-            {
-                title: 'Reseñas',
-                icon: '1.TH.DASHBOARD',
-                include : ['-'],
-                url : '/dashboard'
-            },
-            {
-                title: 'Análisis',
-                icon: '1.TH.DASHBOARD',
-                include : ['-'],
-                url : '/dashboard'
-            },
-            
+  {
+    title: 'Operación',
+    group: [
+      {
+        title: 'Estancias',
+        icon: '1.TH.DASHBOARD',
+        include: ['-'],
+        url: '/dashboard',
+      },
+      {
+        title: 'Reseñas',
+        icon: '1.TH.DASHBOARD',
+        include: ['-'],
+        url: '/dashboard',
+      },
+      {
+        title: 'Análisis',
+        icon: '1.TH.DASHBOARD',
+        include: ['-'],
+        url: '/dashboard',
+      },
+    ],
+  },
+  {
+    title: 'Administración',
+    group: [
+      {
+        title: 'WebApp',
+        icon: '1.TH.DASHBOARD',
+        include: [
+          'ReviewRequestSettingsIndex',
+          'SettingsPreStayPage',
+          'StayPage',
+          'SettingsPostStayPage',
         ],
-    },
-    {
-        title: 'Administración',
-        group: [
-            {
-                title: 'WebApp',
-                icon: '1.TH.DASHBOARD',
-                include : ['ReviewRequestSettingsIndex','SettingsPreStayPage','StayPage','SettingsPostStayPage'],
-                url : '/dashboard'
-            },
-            {
-                title: 'Comunicaciones',
-                icon: '1.TH.DASHBOARD',
-                include : ['-'],
-                url : '/dashboard'
-            },
-            {
-                title: 'Plataformas externas',
-                icon: '1.TH.DASHBOARD',
-                include : ['-'],
-                url : '/dashboard'
-            },
-            {
-                title: 'Datos',
-                icon: '1.TH.DASHBOARD',
-                include : ['-'],
-                url : '/dashboard'
-            },
-            {
-                title: 'Equipo',
-                icon: '1.TH.DASHBOARD',
-                include : ['UserNotificationsSettings','UsersSettings'],
-                url : '/usuarios/configuracion/usuarios'
-            },
-        ],
-    },  
+        url: '/dashboard',
+      },
+      {
+        title: 'Comunicaciones',
+        icon: '1.TH.DASHBOARD',
+        include: ['-'],
+        url: '/dashboard',
+      },
+      {
+        title: 'Plataformas externas',
+        icon: '1.TH.DASHBOARD',
+        include: ['-'],
+        url: '/dashboard',
+      },
+      {
+        title: 'Datos',
+        icon: '1.TH.DASHBOARD',
+        include: ['-'],
+        url: '/dashboard',
+      },
+      {
+        title: 'Equipo',
+        icon: '1.TH.DASHBOARD',
+        include: ['UserNotificationsSettings', 'UsersSettings'],
+        url: '/usuarios/configuracion/usuarios',
+      },
+    ],
+  },
 ])
 
 const user_buttons = ref([
-    {
-        title: 'Ayuda',
-        icon: '/assets/icons/1.TH.AYUDA.MM.svg',
-        include : ['-'],
+  {
+    title: 'Ayuda',
+    icon: '/assets/icons/1.TH.AYUDA.MM.svg',
+    include: ['-'],
+  },
+  {
+    title: 'Novedades',
+    icon: '/assets/icons/1.TH.NOVEDADES.MM.svg',
+    include: ['-'],
+  },
+  {
+    title: `${authStore.user.name} ${authStore.user.lastname}`,
+    icon: `https://ui-avatars.com/api/?name=${authStore.user.name}&color=fff&background=${authStore.user.color}`,
+    include: ['UserPanel'],
+    border: true,
+    action: () => {
+      modalProfile.value = true
     },
-    {
-        title: 'Novedades',
-        icon: '/assets/icons/1.TH.NOVEDADES.MM.svg',
-        include : ['-'],
-    },
-    {
-        title: authStore.user.name+' '+authStore.user.lastname,
-        icon: `https://ui-avatars.com/api/?name=${authStore.user.name}&color=fff&background=${authStore.user.color}`,
-        include : ['-'],
-        border : true,
-        action : () => {
-            modalProfile.value = true
-        }
-    },
-    /* {
-        title: 'Cerrar sesión',
-        icon: null, 
-        include : ['-'],
-        action: async () => {
-            await authStore.logout() 
-        }
-    } */
+    active: computed(() => route.name === 'UserPanel'),
+  },
 ])
 
-const displayedMenu = computed(() => route.meta.displayedMenu ?? false);
+const displayedMenu = computed(() => route.meta.displayedMenu ?? false)
 const widthMenu = computed(() => {
-    let withStyles = 'w-0 group-hover:w-[188px] overflow-hidden transition-all duration-500 ease-in-out';
-    if(displayedMenu.value){
-        withStyles = 'w-[188px]';
-    }
-    return withStyles;
-});
+  let withStyles =
+    'w-0 group-hover:w-[188px] overflow-hidden transition-all duration-500 ease-in-out'
+  if (displayedMenu.value) {
+    withStyles = 'w-[188px]'
+  }
+  return withStyles
+})
 
 const logout = async () => {
   await closeModalProfile()
@@ -237,6 +240,12 @@ const logout = async () => {
 
 <style scoped>
 .modal-button:hover .icon-hover {
-  filter: invert(41%) sepia(58%) saturate(3098%) hue-rotate(127deg) brightness(91%) contrast(91%);
+  filter: invert(41%) sepia(58%) saturate(3098%) hue-rotate(127deg) brightness(91%) contrast(91%)
+}
+.hbg-green-200 {
+  background-color: #E2F8F2;
+}
+.hbg-green-600 {
+  background-color: #34A98F;
 }
 </style>
