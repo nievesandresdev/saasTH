45a18e22642a2b198c877fe9cd77178ff2e4a707
Fran <francisco20990@gmail.com>
2024-07-16 15:53:22 -0400

    platforms external data url in front

--- src/Modules/User/Settings/ExternalPlatforms.vue
+++ src/Modules/User/Settings/ExternalPlatforms.vue
@@ -17,68 +17,80 @@
             <div class="font-montserrat">
                 <div class="w-full h-min mb-4 px-4 py-6 bg-white shadow-md rounded-lg flex flex-col gap-2">
                     <span class="font-medium text-sm">Alojamiento 1</span>
-                    <div :class="['flex', hoverValidation ? 'border-green-500' : 'border-gray-300']">
-                        <span :class="['inline-flex items-center px-2 text-sm text-gray-900 border border-e-0 pr-6', hoverValidation ? 'border-green-500' : 'border-gray-300', 'rounded-s-md']">
+                    <div :class="['flex', hoverValidation.booking ? 'border-green-500' : 'border-gray-300']">
+                        <span :class="['inline-flex items-center px-2 text-sm text-gray-900 border border-e-0 pr-6', hoverValidation.booking ? 'border-green-500' : 'border-gray-300', 'rounded-s-md']">
                             <img src="/assets/icons/otas/Booking.svg" class="w-8 h-8 mr-2" alt="Booking">
                             <span class="font-semibold text-sm">Booking</span>
                         </span>
-                        <input type="text" v-model="form.booking" :class="['rounded-none rounded-e-lg border block flex-1 min-w-0 w-full text-sm p-2.5 placeholder:text-[#A0A0A0] placeholder:font-medium placeholder:text-[14px]', hoverValidation ? 'border-green-500 focus:ring-green-500' : 'border-gray-300 focus:ring-green-500']" placeholder="Escribe URL">
+                        <input type="text" v-model="form.booking" :class="['rounded-none rounded-e-lg border block flex-1 min-w-0 w-full text-sm p-2.5', hoverValidation.booking ? 'border-green-500 focus:ring-green-500 text-green-500' : 'border-gray-300 focus:ring-green-500 placeholder:text-[#A0A0A0] placeholder:font-medium placeholder:text-[14px]']" placeholder="Escribe URL" @input="marcarCambio('booking')">
                     </div>
                 </div>
                 <div class="w-full h-min mb-4 px-4 py-6 bg-white shadow-md rounded-lg flex flex-col gap-2">
                     <span class="font-medium text-sm">Alojamiento 1</span>
-                    <div :class="['flex', hoverValidation ? 'border-green-500' : 'border-gray-300']">
-                        <span :class="['inline-flex items-center px-2 text-sm text-gray-900 border border-e-0', hoverValidation ? 'border-green-500' : 'border-gray-300', 'rounded-s-md']">
+                    <div :class="['flex', hoverValidation.tripadvisor ? 'border-green-500' : 'border-gray-300']">
+                        <span :class="['inline-flex items-center px-2 text-sm text-gray-900 border border-e-0', hoverValidation.tripadvisor ? 'border-green-500' : 'border-gray-300', 'rounded-s-md']">
                             <img src="/assets/icons/otas/Tripadvisor.svg" class="w-8 h-8 mr-2" alt="Tripadvisor">
                             <span class="font-semibold text-sm">Tripadvisor</span>
                         </span>
-                        <input type="text" v-model="form.tripadvisor" :class="['rounded-none rounded-e-lg border block flex-1 min-w-0 w-full text-sm p-2.5 placeholder:text-[#A0A0A0] placeholder:font-medium placeholder:text-[14px]', hoverValidation ? 'border-green-500 focus:ring-green-500' : 'border-gray-300 focus:ring-green-500']" placeholder="Escribe URL">
+                        <input type="text" v-model="form.tripadvisor" :class="['rounded-none rounded-e-lg border block flex-1 min-w-0 w-full text-sm p-2.5', hoverValidation.tripadvisor ? 'border-green-500 focus:ring-green-500 text-green-500' : 'border-gray-300 focus:ring-green-500 placeholder:text-[#A0A0A0] placeholder:font-medium placeholder:text-[14px]']" placeholder="Escribe URL" @input="marcarCambio('tripadvisor')">
                     </div>
                 </div>
                 <div class="w-full h-min mb-4 px-4 py-6 bg-white shadow-md rounded-lg flex flex-col gap-2">
                     <span class="font-medium text-sm">Alojamiento 1</span>
-                    <div :class="['flex', hoverValidation ? 'border-green-500' : 'border-gray-300']">
-                        <span :class="['inline-flex items-center px-2 text-sm text-gray-900 border border-e-0 pr-6', hoverValidation ? 'border-green-500' : 'border-gray-300', 'rounded-s-md']">
+                    <div :class="['flex', hoverValidation.expedia ? 'border-green-500' : 'border-gray-300']">
+                        <span :class="['inline-flex items-center px-2 text-sm text-gray-900 border border-e-0 pr-6', hoverValidation.expedia ? 'border-green-500' : 'border-gray-300', 'rounded-s-md']">
                             <img src="/assets/icons/otas/Expedia.svg" class="w-8 h-8 mr-2" alt="Expedia">
                             <span class="font-semibold text-sm">Expedia</span>
                         </span>
-                        <input type="text" v-model="form.expedia" :class="['rounded-none rounded-e-lg border block flex-1 min-w-0 w-full text-sm p-2.5 placeholder:text-[#A0A0A0] placeholder:font-medium placeholder:text-[14px]', hoverValidation ? 'border-green-500 focus:ring-green-500' : 'border-gray-300 focus:ring-green-500']" placeholder="Escribe URL">
+                        <input type="text" v-model="form.expedia" :class="['rounded-none rounded-e-lg border block flex-1 min-w-0 w-full text-sm p-2.5', hoverValidation.expedia ? 'border-green-500 focus:ring-green-500 text-green-500' : 'border-gray-300 focus:ring-green-500 placeholder:text-[#A0A0A0] placeholder:font-medium placeholder:text-[14px]']" placeholder="Escribe URL" @input="marcarCambio('expedia')">
                     </div>
                 </div>
                 <div class="w-full h-min mb-4 px-4 py-6 bg-white shadow-md rounded-lg flex flex-col gap-2">
                     <span class="font-medium text-sm">Alojamiento 1</span>
-                    <div :class="['flex', hoverValidation ? 'border-green-500' : 'border-gray-300']">
-                        <span :class="['inline-flex items-center px-2 text-sm text-gray-900 border border-e-0 pr-6', hoverValidation ? 'border-green-500' : 'border-gray-300', 'rounded-s-md']">
+                    <div :class="['flex', hoverValidation.google ? 'border-green-500' : 'border-gray-300']">
+                        <span :class="['inline-flex items-center px-2 text-sm text-gray-900 border border-e-0 pr-6', hoverValidation.google ? 'border-green-500' : 'border-gray-300', 'rounded-s-md']">
                             <img src="/assets/icons/otas/Google.svg" class="w-8 h-8 mr-2" alt="Google">
                             <span class="font-semibold text-sm">Google</span>
                         </span>
-                        <input type="text" v-model="form.google" :class="['rounded-none rounded-e-lg border block flex-1 min-w-0 w-full text-sm p-2.5 placeholder:text-[#A0A0A0] placeholder:font-medium placeholder:text-[14px]', hoverValidation ? 'border-green-500 focus:ring-green-500' : 'border-gray-300 focus:ring-green-500']" placeholder="Escribe URL">
+                        <input type="text" v-model="form.google" :class="['rounded-none rounded-e-lg border block flex-1 min-w-0 w-full text-sm p-2.5', hoverValidation.google ? 'border-green-500 focus:ring-green-500 text-green-500' : 'border-gray-300 focus:ring-green-500 placeholder:text-[#A0A0A0] placeholder:font-medium placeholder:text-[14px]']" placeholder="Escribe URL" @input="marcarCambio('google')">
                     </div>
                 </div>
             </div>
+            <ModalNoSave
+                :id="'not-saved'"
+                :open="true"
+                text="Tienes cambios sin guardar. Para aplicar los cambios realizados debes guardar."
+                textbtn="Guardar"
+                @saveChanges="submit"
+                type="save_changes"
+            />
         </div> 
         <ChangesBar 
             :existingChanges="changes > 0" :validChanges="valid"
-            @cancel="cancelChanges" @submit="submit"
+            @cancel="cancelarCambios" @submit="submit"
         />
     </div>
 </template>
 
 <script setup>
 import { ref, computed, watch, onMounted } from 'vue';
-import { useAuthStore } from '@/stores/modules/auth/login';
 import { useUserStore } from '@/stores/modules/users/users';
 import { useToastAlert } from '@/composables/useToastAlert';
 import { useRouter } from 'vue-router';
 import Tooltip from '@/components/Tooltip.vue';
 import ChangesBar from '@/components/Forms/ChangesBar.vue';
+import ModalNoSave from '@/components/ModalNoSave.vue';
 
-const authStore = useAuthStore();
 const userStore = useUserStore();
 const toast = useToastAlert();
 const router = useRouter();
 
-const hoverValidation = ref(false);
+const hoverValidation = ref({
+    booking: false,
+    tripadvisor: false,
+    expedia: false,
+    google: false
+});
 const changes = ref(0);
 
 const form = ref({
@@ -89,14 +101,52 @@ const form = ref({
 });
 
 const submit = () => {
-    alert('Changes submitted');
+    alert('Cambios guardados');
+    changes.value = 0;
 }
 
-const cancelChanges = () => {
-    alert('Changes canceled');
+const cancelarCambios = () => {
+    alert('Cambios cancelados');
+    changes.value = 0;
 }
 
 const valid = computed(() => {
     return changes.value > 0;
+});
+
+const marcarCambio = (field) => {
+    changes.value += 1;
+    hoverValidation.value[field] = form.value[field].length > 0;
+};
+
+onMounted(async () => {
+    try {
+        const response = await userStore.$getDataOTAS();
+        if (response && response.data && response.data.otas.otas) {
+            response.data.otas.otas.forEach(ota => {
+                switch(ota.ota.toLowerCase()) {
+                    case 'tripadvisor':
+                        form.value.tripadvisor = ota.url;
+                        hoverValidation.value.tripadvisor = ota.url.length > 0;
+                        break;
+                    case 'expedia':
+                        form.value.expedia = ota.url;
+                        hoverValidation.value.expedia = ota.url.length > 0;
+                        break;
+                    case 'booking':
+                        form.value.booking = ota.url;
+                        hoverValidation.value.booking = ota.url.length > 0;
+                        break;
+                    case 'google':
+                        form.value.google = ota.url;
+                        hoverValidation.value.google = ota.url.length > 0;
+                        break;
+                }
+            });
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        // El formulario se queda vacío si hay un error
+    }
 });
 </script>
