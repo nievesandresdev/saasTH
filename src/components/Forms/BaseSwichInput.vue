f50adfda5308146044470fedd75bdf1e36ecfa4f
jhjacoz96 <jhjacoz96@gmail.com>
2024-09-18 15:38:31 -0400

    fix

--- src/components/Forms/BaseSwichInput.vue
+++ src/components/Forms/BaseSwichInput.vue
@@ -10,7 +10,7 @@
             @click="handleCLick"
         />
         <div
-        :class="['block w-[1.875rem] h-[1.125rem] rounded-full', modelValue ? 'bg-[#34A98F]' : `bg-[${colorDisabled}]`]"
+        :class="['block w-[1.875rem] h-[1.125rem] rounded-full', modelValue ? 'bg-[#34A98F]' : `bg-[#FFC506]`]"
         ></div>
         <div  
           :class="['dot absolute left-[0.125rem] top-[0.125rem] w-3.5 h-3.5 rounded-full transition ', modelValue ? 'translate-x-[0.75rem] bg-white' : 'bg-white']"
