df9d68fcf5422f68c603c1aec480e60461372233
Fran <francisco20990@gmail.com>
2025-02-11 07:01:46 -0400

    fix

--- src/stores/modules/mockup.js
+++ src/stores/modules/mockup.js
@@ -20,7 +20,7 @@ export const useMockupStore = defineStore('mockupStore', () => {
       let subdomainChain = hotelStore?.hotelData?.chain?.subdomain;
       let slugHotel = hotelStore?.hotelData?.subdomain;
       let urlBase = $urlBaseWebapp(subdomainChain, slugHotel);
-      // console.log(subdomain)
+      console.log(urlBase,subdomainChain,slugHotel)
       // if(ENVIROMENT == 'test'){
       //   urlBase = `https://${subdomain}.test.thehoster.io/webapp`;
       // }
