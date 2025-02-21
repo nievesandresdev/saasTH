edd486759b83e6bfa1fa14ad1ffdef0d046522dc
jhjacoz96 <jhjacoz96@gmail.com>
2025-02-21 08:50:08 -0400

    fix

--- src/components/BaseStepper.vue
+++ src/components/BaseStepper.vue
@@ -8,13 +8,10 @@
             <!-- Número del paso -->
             <div
                 class="cursor-pointer"
-                :class="['w-10 h-10 rounded-full flex items-center justify-center text-lg font-semibold z-10',
-                modelValue == index ? 'bg-[#34A98F] text-white' : 'bg-white text-black border border-gray-300'
-                
-                ]"
+                :class="['w-10 h-10 rounded-full flex items-center justify-center text-lg font-semibold z-10', (modelValue >= index)  ? 'bg-[#34A98F] text-white' : 'bg-white text-black border border-gray-300']"
                 @click="nextStep(index)"
             >
-            {{ index+1 }}
+            {{ index+1 }} 
             </div>
 
             <!-- Etiqueta del paso -->
@@ -28,15 +25,18 @@
             </span>
 
             <!-- Línea de conexión entre pasos -->
+        <!-- <div
+            class="absolute h-[4px] border top-0"
+            :class="modelValue == index ? 'border-green-400' : 'border-grey-300'"
+            :style="{ width: 'calc(100% - 150px)', left: '75px' }"
+        /> -->
         </div>
-        <div 
+        <div
             v-if="index !== steps.length - 1"
-            class="border-gray-300"
-            :class="[ 
-                'absolute h-0 border top-5',
-            ]"
-            :style="{ width: 'calc(100% - 150px)', left: '80px' }"
-        ></div>
+            class=" absolute h-0 border top-5"
+            :class="modelValue >= steps.length - 1 ? 'border-[#34A98F]' : 'border-grey-300'"
+            :style="{ width: 'calc(100% - 150px)', left: '75px' }"
+        />
     </div>
 </template>
 
