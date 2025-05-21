fbdd98ba6b35b4f62e5a3b7a9f92048743150c8c
jhjacoz96 <jhjacoz96@gmail.com>
2025-05-21 18:44:07 -0400

    fix

--- src/api/services/facility.service.js
+++ src/api/services/facility.service.js
@@ -5,7 +5,7 @@ import {
 
 export const getAllApi = (params) => apiHttp('GET', `${facilityPath}/hoster/getAll`, params, {showPreloader: false });
 export const findByIdApi = (id) => apiHttp('GET', `${facilityPath}/findById/${id}`);
-export const updateOrderApi = (data) => apiHttp('POST', `${facilityPath}/order`, data, {showPreloader: false }, 'API_HELPER', false, true);
-export const updateVisibleApi = (data) => apiHttp('POST', `${facilityPath}/visible`, data, {showPreloader: false }, 'API_HELPER', false, true);
-export const storeOrUpdateApi = (data) => apiHttp('POST', `${facilityPath}/storeOrUpdate`, data, {showPreloader: false }, 'API_HELPER', false, true);
-export const deleteApi = (id) => apiHttp('DELETE', `${facilityPath}/${id}`, {showPreloader: false }, 'API_HELPER', false, true);
+export const updateOrderApi = (data) => apiHttp('POST', `${facilityPath}/order`, data, {showPreloader: false }, 'API_GENERAL', false, true);
+export const updateVisibleApi = (data) => apiHttp('POST', `${facilityPath}/visible`, data, {showPreloader: false }, 'API_GENERAL', false, true);
+export const storeOrUpdateApi = (data) => apiHttp('POST', `${facilityPath}/storeOrUpdate`, data, {showPreloader: false }, 'API_GENERAL', false, true);
+export const deleteApi = (id) => apiHttp('DELETE', `${facilityPath}/${id}`, {showPreloader: false }, 'API_GENERAL', false, true);
